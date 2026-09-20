28dae7cc6d8167d844b3c25dae9df9bc67880111
Abhishek Srivastava <abhishek0wb@gmail.com>
2026-09-20 17:55:52 +0530

    WebSocket with internally polls Redis

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect
 from arq import create_pool
 from arq.connections import RedisSettings
 from contextlib import asynccontextmanager
+import asyncio
 
 REDIS = RedisSettings(host="localhost", port=6379)
 
@@ -12,6 +13,26 @@ async def lifespan(app: FastAPI):
     await app.state.redis.close()
 
 app = FastAPI(lifespan=lifespan)
+
+@app.websocket("/ws/task/{task_id}")
+async def task_progress_ws(websocket:  WebSocket, task_id:str):
+    await websocket.accept()
+    try:
+        last_sent = None
+        while True:
+            percent = await app.state.redis.get(f"progress:{task_id}")
+            percent = int(percent) if percent is not None else 0
+
+            if percent != last_sent:
+                await websocket.send_json({"task_id": task_id, "progress": percent})
+                last_sent = percent
+
+            if percent >= 100:
+                break
+
+            await asyncio.sleep(0.5)
+    except WebSocketDisconnect:
+            pass            
 
 @app.post("/task/submit")
 async def submit_task(total_steps:int = 10):
