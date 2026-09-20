e943794f66870523df52068218caff97649ee805
Abhishek Srivastava <abhishek0wb@gmail.com>
2026-09-20 17:15:21 +0530

    lifespan added for a single redis connection for whole app

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI, HTTPException
 from arq import create_pool
 from arq.connections import RedisSettings
+from contextlib import asynccontextmanager
 
-app = FastAPI()
 REDIS = RedisSettings(host="localhost", port=6379)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    app.state.redis = await create_pool(REDIS)
+    yield
+    await app.state.redis.close()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.post("/task/submit")
 async def submit_task(total_steps:int = 10):
-    redis = await create_pool(REDIS)
-    job = await redis.enqueue_job("process_fake_job", total_steps)
+    job = await app.state.redis.enqueue_job("process_fake_job", total_steps)
     return {"task_id": job.job_id}
 
 @app.get("/task/{task_id}")
 async def get_progress(task_id: str):
-    redis = await create_pool(REDIS)
-    percent = await redis.get(f"progress:{task_id}")
+    percent = await app.state.redis.get(f"progress:{task_id}")
     if percent is None:
         return {"task_id": task_id, "progress": 0 , "note": "queued or unknow"}
     return {"task_id": task_id, "progress": int(percent)}        
